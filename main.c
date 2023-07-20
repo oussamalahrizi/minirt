0c169b75595fbdfbb7eac9860b49088b4b7aa35c
--list <olahrizi@e1r12p3.1337.ma>
2023-07-20 14:37:06 +0100

    test

--- main.c
+++ main.c
@@ -3,12 +3,8 @@
 
 unsigned int rgbToInteger(t_color color)
 {
-	unsigned int intRed = (unsigned int)(color.red);
-    unsigned int intGreen = (unsigned int)(color.green);
-    unsigned int intBlue = (unsigned int)(color.blue);
-    unsigned int intAlpha = 0;
 
-    unsigned int result = (intAlpha << 24) | (intRed << 16) | (intGreen << 8) | intBlue;
+    unsigned int result = (color.red << 16) | (color.green << 8) | color.blue;
     return result;
 }
 
@@ -21,8 +17,6 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, t_color color)
 	*(unsigned int*)dst = rgbToInteger(color);
 }
 
-
-
 void		ray_intersects_sphere(double radius, t_ray *ray, t_sphere sphere, t_color *pixe_color)
 {
 	// t_vec3 *lightdir = new_vector3(-40.0,50.0,0.0);
@@ -32,6 +26,7 @@ void		ray_intersects_sphere(double radius, t_ray *ray, t_sphere sphere, t_color 
 	double c;
 	double delta;
 	double solution;
+	// t_vec3 *sphere_pos = new_vector3(sphere.center.x * 2 -1, sphere.center.y * 2 -1, sphere.center.z * 2 -1);
 	t_vec3 *oc = vec3_sub(ray->origin, &sphere.center);
 	// set quadtratic formula parameters
 	a = dot_product(ray->direction, ray->direction);
@@ -42,26 +37,20 @@ void		ray_intersects_sphere(double radius, t_ray *ray, t_sphere sphere, t_color 
 	if (delta >= 0)
 	{
 		// we take the smallest solution because it's the first hitpoint since we cant see from our pov the other hitpoint
-		solution = (-b - sqrt(delta))/ 2 * a;
+		solution = (-(b / 2) - sqrt(delta)) / a;
 		t_vec3 *hitposition = malloc(sizeof(t_vec3));
-		hitposition->x = ray->origin->x + (ray->direction->x * solution);
-		hitposition->y = ray->origin->y +( ray->direction->y * solution);
-		hitposition->z = ray->origin->z + (ray->direction->z * solution);
-		t_vec3 *normal = normalized(lightdir);
-		double light = max(dot_product(normal, negative_vec3(lightdir)), 0);
-		t_vec3 *sphere_color = new_vector3(sphere.color.red, sphere.color.green, sphere.color.blue);
-		t_vec3 *multi = multiply_vec3_number(sphere_color, light);
-		// pixe_color->red = ((int)(multi->x * 0.5 + 0.5)) * 255;
-		// pixe_color->green = ((int)(multi->y * 0.5 + 0.5)) * 255;
-		// pixe_color->blue = ((int)(multi->z * 0.5 + 0.5)) * 255;
-		// pixe_color->red = (normal->x * 0.5 + 0.5) * 255;
-		// pixe_color->green = (normal->y * 0.5 + 0.5) * 255;
-		// pixe_color->blue = (normal->z * 0.5 + 0.5) * 255;
-		hitposition = normalized(hitposition);
-		pixe_color->red = ((hitposition->x * 0.5 + 0.5)) * 255;
-		pixe_color->green = ((hitposition->y * 0.5 + 0.5)) * 255;
-		pixe_color->blue = ((hitposition->z * 0.5 + 0.5)) * 255;
-		} 
+		hitposition->x = oc->x + (ray->direction->x * solution);
+		hitposition->y = oc->y +( ray->direction->y * solution);
+		hitposition->z = oc->z + (ray->direction->z * solution);
+		t_vec3 *normal = normalized(hitposition);
+		lightdir = normalized(lightdir);
+		double d = max(dot_product(normal, negative_vec3(lightdir)), 0);
+		t_vec3 *sphere_color = new_vector3((double)sphere.color.red / 255, (double)sphere.color.green / 255, (double)sphere.color.blue / 255);
+		t_vec3 *multi = multiply_vec3_number(sphere_color, d);
+		pixe_color->red = (int)(multi->x * 255 + 0.5);
+		pixe_color->green = (int)(multi->y * 255 + 0.5);
+		pixe_color->blue = (int)(multi->z * 255 + 0.5);
+	} 
 }
 
 t_color	trace_ray(t_ray *ray, t_scene scene)
@@ -75,10 +64,6 @@ t_color	trace_ray(t_ray *ray, t_scene scene)
 	return (pixel_color);
 }
 
-double degrees_to_radians(double degrees)
-{
-    return degrees * M_PI / 180.0;
-}
 
 void	render_image(t_scene scene, void *mlx_ptr, void *win_ptr, t_data *data)
 {
@@ -88,10 +73,10 @@ void	render_image(t_scene scene, void *mlx_ptr, void *win_ptr, t_data *data)
 	t_color	pixel_color;
 	t_vec2 coord;
 
-	y = 0;
 	ray = malloc(sizeof(t_ray));
-	double fov = 70;
 	ray->origin = new_vector3(0 , 0 , -1);
+	// ray->origin = new_vector3(ray->origin->x * 2 - 1, ray->origin->y * 2 - 1, ray->origin->z * 2 - 1);
+	y = 0;
 	while (y < HEIGHT)
 	{
 		x = 0;
@@ -99,13 +84,13 @@ void	render_image(t_scene scene, void *mlx_ptr, void *win_ptr, t_data *data)
 		{
 			// this calculations to make the ray hit the center of the pixel 
 			coord.x = (double) x / WIDTH;
-			coord.y = (double) y / WIDTH;
+			coord.y = (double) y / HEIGHT;
 			coord.x = coord.x * 2 - 1;
 			coord.y = coord.y * 2 - 1;
 			// coord.x = (double)x / (double)(WIDTH - 1);
             // coord.y = (double)y / (double)(HEIGHT - 1);
 			ray->direction = new_vector3(coord.x, coord.y, -1); // orientation vector , look subject
-			// i dont normalize cuz it affets performance hehe
+			// i dont normalize cuz it affets performance hehe 
 			pixel_color = trace_ray(ray, scene);
 			// put_pixel_to_image(data->img, x, y, pixel_color);
 			my_mlx_pixel_put(data, x, y, pixel_color);
@@ -130,7 +115,7 @@ int		main(void)
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
 	scene.sphere.center = (t_vec3){0, 0, 0}; // Sphere position
 	scene.sphere.radius = 0.5; // Sphere radius
-	scene.sphere.color = (t_color){10,0,255}; // Sphere color according to subject
+	scene.sphere.color = (t_color){255,0,255}; // Sphere color according to subject
 	render_image(scene, mlx_ptr, win_ptr, &img);
 	mlx_loop(mlx_ptr);
 	return (0);
